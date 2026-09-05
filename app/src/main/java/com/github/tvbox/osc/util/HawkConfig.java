d25dfab5f7af04448e69ae0757be014d36d2eaaa
tytestelle <ccszdk@gmail.com>
2026-09-05 20:25:05 +0800

    Update HawkConfig.java

--- app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/HawkConfig.java
@@ -24,6 +24,7 @@ public class HawkConfig {
     public static final String LIVE_PLAY_TYPE = "live_play_type";    // 0 系统 1 ijk 2 exo 10 MXPlayer
     public static final String LIVE_PLAY_SCALE = "live_play_scale";
     public static final String LIVE_SOURCE_NAME = "live_source_name"; // 新增：当前加载的直播源名称
+    public static final String LIVE_SOURCE_LIST = "live_source_list";
 
     // EPG相关（修正了重复定义）
     public static final String EPG_URL = "epg_url";                  // EPG订阅地址
