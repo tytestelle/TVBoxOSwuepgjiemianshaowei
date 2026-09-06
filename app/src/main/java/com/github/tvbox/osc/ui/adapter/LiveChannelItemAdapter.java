22e7fb1416e9f47484830b1d288aa7909d144e93
tytestelle <ccszdk@gmail.com>
2026-09-06 18:44:20 +0800

    Update LiveChannelItemAdapter.java

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveChannelItemAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/LiveChannelItemAdapter.java
@@ -1,19 +1,23 @@
 package com.github.tvbox.osc.ui.adapter;
 
 import android.graphics.Color;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.github.tvbox.osc.R;
 import com.github.tvbox.osc.bean.LiveChannelItem;
+import com.github.tvbox.osc.logo.LogoManager; // 请根据实际包名调整
 
+import java.io.File;
 import java.util.ArrayList;
 
 /**
  * @author pj567
  * @date :2021/1/12
- * @description:
+ * @description: 频道列表适配器（含台标异步加载）
  */
 public class LiveChannelItemAdapter extends BaseQuickAdapter<LiveChannelItem, BaseViewHolder> {
     private int selectedChannelIndex = -1;
@@ -25,19 +29,47 @@ public class LiveChannelItemAdapter extends BaseQuickAdapter<LiveChannelItem, Ba
 
     @Override
     protected void convert(BaseViewHolder holder, LiveChannelItem item) {
+        // 原有视图绑定
         TextView tvChannelNum = holder.getView(R.id.tvChannelNum);
         TextView tvChannel = holder.getView(R.id.tvChannelName);
         tvChannelNum.setText(String.format("%s", item.getChannelNum()));
         tvChannel.setText(item.getChannelName());
         tvChannelNum.setSelected(true);
         tvChannel.setSelected(true);
+
+        // ---------- 新增：台标加载 ----------
+        ImageView ivLogo = holder.getView(R.id.ivChannelLogo); // 请确保布局中存在该id
+        // 先设置默认占位（可自定义）
+        Glide.with(mContext).load(R.drawable.default_channel_logo).into(ivLogo);
+
+        String channelName = item.getChannelName();
+        String channelLogoUrl = item.getChannelLogo(); // 假设LiveChannelItem有getChannelLogo()方法
+
+        // 异步下载台标
+        LogoManager.getInstance(mContext).downloadLogo(channelName, channelLogoUrl, new LogoManager.LogoCallback() {
+            @Override
+            public void onSuccess(File file) {
+                // 在UI线程加载图片
+                if (holder.getLayoutPosition() >= 0) { // 简单检查item是否仍然有效
+                    Glide.with(mContext).load(file).into(ivLogo);
+                }
+            }
+
+            @Override
+            public void onError(String msg) {
+                // 加载失败保留默认占位
+                // 可在此记录日志
+            }
+        });
+        // ---------- 新增结束 ----------
+
+        // 原有选中/焦点颜色逻辑
         int channelIndex = item.getChannelIndex();
         holder.itemView.setSelected(channelIndex == selectedChannelIndex);
         if (channelIndex == selectedChannelIndex && channelIndex != focusedChannelIndex) {
             tvChannelNum.setTextColor(mContext.getResources().getColor(R.color.color_1890FF));
             tvChannel.setTextColor(mContext.getResources().getColor(R.color.color_1890FF));
-        }
-        else{
+        } else {
             tvChannelNum.setTextColor(Color.WHITE);
             tvChannel.setTextColor(Color.WHITE);
         }
